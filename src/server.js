de094e40f08b1932f50b9cbec117025b9abd8006
luan <xoaygio2001@gmail.com>
2024-06-08 14:00:09 +0700

    update

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 let app = express();
 
-app.use(cors({origin: true, credentials: true}));
+app.use(cors({origin: true}));
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
